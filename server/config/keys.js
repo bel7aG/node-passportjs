bcecb77ebeac12bfec95e4ee20e5803ec0e88ec2
Belhassen Gharsallah <Belhassen.Gharsallah@gmail.com>
2019-03-04 21:59:03 +0100

    google starategy

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientID: `732039042054-gl75c0vkkulglpnq3e7kmhuct5tgnv1h.apps.googleusercontent.com`,
+  googleClientSecret: `HMyd4DfZWcchB-llPLYm7E2u`
+};
--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
-const express = require(`express`);
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
-app.get(`/`, (request, response) => {
-  response.send({
-    learn: "coding express for the first time"
-  });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 const PORT = process.env.PORT || 5000; // for deveolepment envirement we use 5000 , for production whatever port heroku give to us
 
